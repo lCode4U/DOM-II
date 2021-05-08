ad2fba44194d04fd5a55891f320a0c31167b0c1e
Torquoya Williams <tlwilliams92tech@yahoo.com>
2021-05-07 20:43:10 -0700

    captured 10 elements

--- js/index.js
+++ js/index.js
@@ -1,4 +1,32 @@
 // Your code goes here
+const docTitle = document.querySelector('title');
+console.log(docTitle);
 
-const mainNav = document.querySelector('.mainNavigation');
+const docBody = document.querySelector('body');
+console.log(docBody);
+
+const mainNav = document.querySelector('.main-navigation')
 console.log(mainNav);
+
+const navCon = mainNav.querySelector('div');
+console.log(navCon);
+
+const docHome = docBody.children[1];
+console.log(docHome);
+
+const docContentAll = docHome.querySelectorAll('section');
+console.log(docContentAll);
+
+const sectionHeaders = document.querySelectorAll('h2');
+console.log(sectionHeaders);
+
+const docImages = document.querySelectorAll('img');
+console.log(docImages);
+
+const allButtons = document.querySelectorAll('.btn');
+console.log(allButtons);
+
+const otherHeaders = document.querySelectorAll('h4');
+console.log(otherHeaders);
+const allParas = docBody.querySelectorAll('p');
+console.log(allParas);
